864d0a74db1fa2ef1a41c49389a11932ca5ce1f6
vagnergros <vagnergrossklaus@gmail.com>
2019-03-17 22:32:58 -0300

    feat: Manage todo list per user 	- Add get method by user and id 	- Add post 	- Add put to edit by user and id 	- Add put as delete (delete return 405 and will be correct in future)

--- app.py
+++ app.py
@@ -11,36 +11,107 @@ class User(object):
         self.tasks = []
 
 class Task(object):
-    def __init__(self, id, name, status):
+    def __init__(self, id, name, state):
         self.id = id
         self.name = name
-        self.status = status
+        self.state = state
 
-@app.route('/todo/tasks/<user_name>', methods=['GET'])
-def get_tasks(user_name):
+def findUser(name):
+    i = 0
+    user = None
+    while i < len(users) and user == None:
+        if (users[i].name == name):
+            user = users[i]
+        i+=1
 
-    task1 = Task(1, "teste", 1)
+    return user
 
-    taskSearizable = json.dumps(task1.__dict__)
+def findTask(user, taskId):
+    i = 0
+    task = None
+    tasks = user.tasks
+    while i < len(tasks) and task == None:
+        if (int(tasks[i].id) == int(taskId)):
+            task = tasks[i]
+        i+=1
+
+    return task
+
+@app.route('/todo/task/<userName>', methods=['GET'])
+def tasks(userName):
+
+    taskSearizable = None
+    user = findUser(userName)
+    if (user != None):
+        taskSearizable = json.dumps([task.__dict__ for task in user.tasks])
+    else:
+        return Response(status=204)
+
+    return Response(taskSearizable,
+                    status=200,
+                    mimetype='application/json')
+
+@app.route('/todo/task/<userName>/<taskId>', methods=['GET'])
+def task(userName, taskId):
+
+    taskSearizable = None
+    user = findUser(userName)
+    if (user != None):
+        task = findTask(user, taskId)
+        if (task != None):
+            taskSearizable = json.dumps(task.__dict__)
+        else:
+            return Response(status=204)
+    else:
+        return Response(status=204)
+
     return Response(taskSearizable,
                     status=200,
                     mimetype='application/json')
 
 @app.route('/todo/task', methods=['POST'])
-def post_task():
+def addTask():
 
     requestBody = request.get_json()
     
-    user = None
     userName = requestBody["name"]
-    indexUser = users.index(userName)
-    if (indexUser >= 0):
+    user = findUser(userName)
+    if user == None:
         user = User(userName)
         users.append(user)
-    else:
-        user = users[indexUser]
 
-    print(request.get_json())
+    tasks = user.tasks
+    taskId = 0
+    if (len(tasks) <= 0):
+        taskId = 1
+    else:
+        taskId = tasks[len(tasks) - 1].id + 1
+    tasks.append(Task(taskId, requestBody["task"]["name"], requestBody["task"]["state"]))
+    return Response(status=200)
+
+@app.route('/todo/task/<taskId>', methods=['PUT'])
+def editTask(taskId):
+
+    requestBody = request.get_json()
+
+    user = findUser(requestBody["name"])
+    if (user != None):
+        task = findTask(user, taskId)
+        if (task != None):
+            task.name = requestBody["task"]["name"]
+            task.state = requestBody["task"]["state"]
+
+    return Response(status=200)
+
+@app.route('/todo/task/<userName>/<taskId>', methods=['PUT'])
+def deleteTask(userName, taskId):
+
+    requestBody = request.get_json()
+
+    user = findUser(userName)
+    if (user != None):
+        task = findTask(user, taskId)
+        user.tasks.remove(task)
 
     return Response(status=200)
 
